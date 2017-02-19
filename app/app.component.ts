4de3fe920d548eca28df73662c74b3ee56f8aa02
o-andres-u <oscarandresu@gmail.com>
2017-02-19 02:56:15 -0500

    Passing attributes between component and front-end

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,19 @@ import {Component} from "angular2/core";
 
 @Component({
   selector: "terminator-app",
-  template: "<h1>Hasta la vista, baby!</h1>"
+  template: "<h1>{{title}}</h1>"
+          + "<ul>"
+            + "<li>Objetivo: {{target}}</li>"
+            + "<li>Arma: {{weapon}}</li>"
+            + "<li>Munición: <strong>{{bullets}}</strong></li>"
+          + "</ul>"
 })
 
-export class AppComponent{}
+export class AppComponent {
+
+  public title = "Terminator Concept";
+  public target = "John Connor";
+  public weapon = "Shotgun";
+  public bullets = 1000;
+
+}
