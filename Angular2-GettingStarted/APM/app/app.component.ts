5e68b0ddf460dfb1718493b257bfb09cb1e4db93
JaviRpo <javier.restrepo.rendon@gmail.com>
2017-02-20 00:44:35 -0500

    Pluralsight Course -> Angular 2: Getting Started Module -> Introduction to Components

---
+++ Angular2-GettingStarted/APM/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-app',
+    template: `
+        <div>
+            <h1>Angular2: Getting Started</h1>
+            <div>My Firt Component</div>
+        </div>
+    `
+})
+export class AppComponent { }
