b2815bb40153d5ab578102ac4fbde0819dc18087
JaviRpo <javier.restrepo.rendon@gmail.com>
2017-02-23 23:41:17 -0500

    Pluralsight Course -> Angular 2: Getting Started Module -> Template, Interpolation, and Directives Video --> Building the Component

---
+++ Angular2-GettingStarted/APM/app/products/product-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "pm-products",
+    templateUrl: "app/products/product-list.component.html"
+})
+export class ProductListComponent {
+
+}
