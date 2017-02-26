c736c05b7f238843fdfb52d179cc0c866efbfe97
JaviRpo <javier.restrepo.rendon@gmail.com>
2017-02-26 01:11:11 -0500

    Pluralsight Course -> Building Mobile Apps with Ionic 2, Angular 2, and TypeScript Module -> Getting Started with Ionic 2
    Fin Módulo

---
+++ elite-schedule-app-v2/src/pages/page1/page1.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-page1',
+  templateUrl: 'page1.html'
+})
+export class Page1 {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+  sayHello() {
+    console.log("Hello, World");
+  }
+}
