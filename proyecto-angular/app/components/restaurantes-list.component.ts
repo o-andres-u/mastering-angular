e8aac34db8050943aa83700f9a5e65cabc0f6b91
JaviRpo <javier.restrepo.rendon@gmail.com>
2017-03-04 14:40:23 -0500

    05 Desarrollando una aplicación web desde cero HTTP Consumir API REST - 040 Crear un componente para el listado de restaurantes.mp4

--- proyecto-angular/app/components/restaurantes-list.component.ts
+++ proyecto-angular/app/components/restaurantes-list.component.ts
@@ -1,17 +1,14 @@
 // Importar el núcleo de Angular
 import {Component} from "angular2/core";
 import {ROUTER_DIRECTIVES, RouteConfig, Router} from "angular2/router";
-import {RestaurantesListComponent} from "./components/restaurantes-list.component";
 
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
-    selector: "my-app",
-    templateUrl: "app/view/home.html",
-    directives: [ ROUTER_DIRECTIVES,
-    RestaurantesListComponent ]
+    selector: "restaurantes-list",
+    templateUrl: "app/view/restaurantes-list.html"
     //, styleUrls: ["../assets/css/styles.css"] //Se coloca en el html para que sea global.
 })
 // Clase del componente donde iran los datos y funcionalidades
-export class AppComponent {
-    public titulo:string = "Restaurantes";
+export class RestaurantesListComponent {
+    public titulo:string = "Listado de Restaurantes";
 }
