2156fffb2c8d20e8b5f849b22fc1109cd05b2e77
JaviRpo <javier.restrepo.rendon@gmail.com>
2017-03-04 14:43:59 -0500

    05 Desarrollando una aplicación web desde cero HTTP Consumir API REST - 041 Crear una clase Restaurante.mp4

---
+++ proyecto-angular/app/model/restaurante.js
@@ -0,0 +1,23 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Restaurante;
+    return {
+        setters:[],
+        execute: function() {
+            Restaurante = (function () {
+                function Restaurante(id, nombre, direccion, descripcion, imagen, precio) {
+                    this.id = id;
+                    this.nombre = nombre;
+                    this.direccion = direccion;
+                    this.descripcion = descripcion;
+                    this.imagen = imagen;
+                    this.precio = precio;
+                }
+                return Restaurante;
+            }());
+            exports_1("Restaurante", Restaurante);
+        }
+    }
+});
+//# sourceMappingURL=restaurante.js.map
