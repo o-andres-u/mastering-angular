37763267d0c870c33535b52403dfbd60c55bf04b
JaviRpo <javier.restrepo.rendon@gmail.com>
2017-03-09 00:54:46 -0500

    Pluralsight Course -> Angular 2: Getting Started Module -> Retrieving Data Using HTTP Video --> Sending an HTTP Request

--- Angular2-GettingStarted/APM/app/app.module.ts
+++ Angular2-GettingStarted/APM/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 
@@ -12,7 +13,8 @@ import { StarComponent } from "./shared/star.component";
 @NgModule({
   imports: [ 
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   declarations: [ 
     AppComponent,
