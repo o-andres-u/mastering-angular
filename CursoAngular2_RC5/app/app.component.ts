3646249d3c565df2f3a231178322bd73d6f07197
Alejandro Palacio <alejopalacioduque@gmail.com>
2017-04-05 15:56:07 -0500

    Capitulo 12 079 Instalar Angular 2 RC 5 y hacer un Hola Mundo

---
+++ CursoAngular2_RC5/app/app.component.ts
@@ -0,0 +1,12 @@
+// Importar el núcleo de Angular
+import {Component} from '@angular/core';
+ 
+// Decorador component, indicamos en que etiqueta se va a cargar la plantilla
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hola mundo con Angular 2</h1>'
+})
+ 
+// Clase del componente donde irán los datos y funcionalidades
+export class AppComponent { }
+
