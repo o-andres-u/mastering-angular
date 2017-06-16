fa81992e34d1b2b19db277e4ae2faf73a7d80b86
Edgar Alejandro Palacio Duque <EPalacioD@pslcol.com.co>
2017-06-16 18:05:47 -0500

    venta pos offline

--- ventaPOS-offline/src/app/ventaPOS/pipe/impuesto-inluido-precio.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImpuestoInluidoPrecioPipe } from './impuesto-inluido-precio.pipe';
-
-describe('ImpuestoInluidoPrecioPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ImpuestoInluidoPrecioPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
